1103d872655a30d9c6ada3257e2ecf490b4f6fc5
Arya Baranwal <baranwalarya414@gmail.com>
2026-08-27 14:22:02 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app=express()
+
+const PORT = process.env.PORT || 6000
+
+app.get("/",(req,res)=>{
+    return res.json({message:"server started"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
