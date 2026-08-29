b466b0277c5a31230c27079db65917be51f7dd0a
Arya Baranwal <baranwalarya414@gmail.com>
2026-08-29 14:53:00 +0530

    new

---
+++ server/config/connectDb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("Database Connected")
+    } catch (error) {
+        console.log(`DB connection error ${error}`)
+    }
+}
+
+export default connectDb
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDb from "./config/connectDb.js"
 
 dotenv.config()
 
@@ -13,4 +14,5 @@ app.get("/",(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
+    connectDb()
 })
